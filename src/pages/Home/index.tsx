624964f1db61ae41093a27ce50c34f4fdba1452f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-06-14 19:58:22 -0300

    added splash screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import { Container, Text } from './styles';
 
 export function Home(): JSX.Element {
   return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Text>Fisio home</Text>
     </Container>
   );
