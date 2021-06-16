e210cdf7ec5eb26d7fa0e04ffd2124e513de7240
João Vitor M Melo <jvgamerofficial@gmail.com>
2021-06-16 18:33:33 -0300

    added login page and header components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,70 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, TouchableWithoutFeedback } from 'react-native';
+import { Header } from '../../components/Header'
 
-import { Container, Text } from './styles';
-
+import { Container, Text,  Main, ScalesContainer, MainTitle, Scale, ScaleTitle, PatientsContainer, Patient, PatientTitle, AddPatientButton, AddPatientIcon } from './styles';
+ 
 export function Home(): JSX.Element {
   return (
     <Container>
+      <Header/>
       <StatusBar
         barStyle="light-content"
         translucent
         backgroundColor="transparent"
-      />
-      <Text>Fisio home</Text>
+      />  
+      <Main>
+        <ScalesContainer>
+          <MainTitle>
+            Escalas e índices
+          </MainTitle>
+          <TouchableWithoutFeedback>
+            <Scale>
+                <ScaleTitle>
+                  MOBILITY SCALE
+                </ScaleTitle>
+            </Scale>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback>
+            <Scale>
+                <ScaleTitle>
+                  ÍNDICE DE BARTHEL
+                </ScaleTitle>
+            </Scale>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback>
+            <Scale>
+                <ScaleTitle>
+                  ESCALA DE MRC
+                </ScaleTitle>
+            </Scale>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback>
+            <Scale>
+                <ScaleTitle>
+                  ÍNDICE DE HACOR
+                </ScaleTitle>
+            </Scale>
+          </TouchableWithoutFeedback>
+        </ScalesContainer>
+        <PatientsContainer>
+          <MainTitle>
+            Pacientes
+          </MainTitle>
+          <TouchableWithoutFeedback>
+            <Patient>
+                <PatientTitle>
+                  A.R.U
+                </PatientTitle>
+            </Patient>
+          </TouchableWithoutFeedback>
+          <AddPatientButton>
+            <TouchableWithoutFeedback>
+              <AddPatientIcon source={require('../../assets/addButton.png')}/>
+            </TouchableWithoutFeedback>
+          </AddPatientButton>
+        </PatientsContainer>
+      </Main>
     </Container>
   );
 }
