4cd38ace78b32248c6fc30cbd01c5db4a48c4465
João Vitor M Melo <jvgamerofficial@gmail.com>
2021-06-19 20:24:04 -0300

    refactor indexBarthel page and added answerModal component

--- src/pages/Indexes/BarthelIndex/index.tsx
+++ src/pages/Indexes/BarthelIndex/index.tsx
@@ -1,34 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, TouchableWithoutFeedback } from 'react-native';
-import Modal from 'react-native-modal';
-import { MaterialIcons } from '@expo/vector-icons';
+import { ScrollView } from 'react-native';
+import Modal from 'react-native-modal'
 
 import { Accordion } from '../../../components/Accordion';
 import { IndexHeader } from '../../../components/IndexHeader';
+import { ScaleAnswerModal } from '../../../components/ScaleAnswerModal';
 
 import {
   IndexContainer,
   AccordionContainer,
   ModalContent,
-  AnswerTitle,
-  SelectAnswerButton,
-  Answer,
   ScoreResultContainer,
   ScoreText,
 } from './styles';
 
 export function BarthelIndex(): JSX.Element {
-  const [alimentationScore, setAlimentationScore] = useState({} as number);
-  const [bathScore, setBathScore] = useState({} as number);
-  const [routineScore, setRoutineScore] = useState({} as number);
-  const [dressUpScore, setDressUpScore] = useState({} as number);
-  const [intestineScore, setIntestineScore] = useState({} as number);
-  const [urinaryScore, setUrinaryScore] = useState({} as number);
+  const [alimentationScore, setAlimentationScore] = useState<number>(0);
+  const [bathScore, setBathScore] = useState<number>(0);
+  const [routineScore, setRoutineScore] = useState<number>(0);
+  const [dressUpScore, setDressUpScore] = useState<number>(0);
+  const [intestineScore, setIntestineScore] = useState<number>(0);
+  const [urinaryScore, setUrinaryScore] = useState<number>(0);
 
   const [barthelScore, setBarthelScore] = useState(0);
 
-  const [isAlimentationModalVisible, setAlimentationModalVisible] =
-    useState(false);
+  const [isAlimentationModalVisible, setAlimentationModalVisible] = useState(false);
   const [isBathModalVisible, setBathModalVisible] = useState(false);
   const [isRoutineModalVisible, setRoutineModalVisible] = useState(false);
   const [isDressUpModalVisible, setDressUpModalVisible] = useState(false);
@@ -121,340 +117,135 @@ export function BarthelIndex(): JSX.Element {
           </ScoreResultContainer>
 
           {/* MODAIS */}
-
           <Modal
             isVisible={isAlimentationModalVisible}
             style={{ margin: 0 }}
             onBackdropPress={() => setAlimentationModalVisible(false)}
           >
             <ModalContent>
-              <Answer>
-                <AnswerTitle>Incapacitado</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
+              <ScaleAnswerModal answerTitle='Incapacitado' onPress={
+                () => {
                     toggleModal('alimentation');
                     setAlimentationScore(0);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>
-                  Precisa de ajuda para cortar, passar manteiga, etc, ou dieta
-                  modificada
-                </AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
+              }} />
+              <ScaleAnswerModal answerTitle='Precisa de ajuda para cortar, passar manteiga, etc, ou dieta
+                  modificada' onPress={() => {
                     toggleModal('alimentation');
                     setAlimentationScore(5);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>Incapacitado</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
+                  }}/>
+                <ScaleAnswerModal answerTitle='Independente' onPress={() => {
                     toggleModal('alimentation');
                     setAlimentationScore(10);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
+                  }} />
             </ModalContent>
           </Modal>
+
           <Modal
             isVisible={isBathModalVisible}
             style={{ margin: 0 }}
             onBackdropPress={() => setBathModalVisible(false)}
           >
-            <ModalContent>
-              <Answer>
-                <AnswerTitle>Dependente</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('bath');
-                    setBathScore(0);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>Independente (ou no chuveiro)</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('bath');
-                    setBathScore(5);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-            </ModalContent>
+              <ModalContent>
+                  <ScaleAnswerModal answerTitle='Dependente' onPress={
+                    () => {
+                        toggleModal('bath');
+                        setBathScore(0);
+                  }} />
+                  <ScaleAnswerModal answerTitle='Independente (ou no chuveiro)' onPress={() => {
+                        toggleModal('bath');
+                        setBathScore(5);
+                      }}/>
+              </ModalContent>
           </Modal>
+
           <Modal
             isVisible={isRoutineModalVisible}
             style={{ margin: 0 }}
             onBackdropPress={() => setRoutineModalVisible(false)}
           >
             <ModalContent>
-              <Answer>
-                <AnswerTitle>
-                  Precisa de ajuda com a higiene pessoal
-                </AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('routine');
-                    setRoutineScore(0);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>
-                  Independente rosto/cabelo/dentes/barbear
-                </AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('routine');
-                    setRoutineScore(5);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
+                <ScaleAnswerModal answerTitle='Precisa de ajuda com a higiene pessoal' onPress={
+                  () => {
+                      toggleModal('routine');
+                      setRoutineScore(0);
+                }} />
+                <ScaleAnswerModal answerTitle='Independente rosto/cabelo/dentes/barbear' onPress={() => {
+                      toggleModal('routine');
+                      setRoutineScore(5);
+                    }}/>
             </ModalContent>
           </Modal>
+
           <Modal
             isVisible={isDressUpModalVisible}
             style={{ margin: 0 }}
             onBackdropPress={() => setDressUpModalVisible(false)}
           >
             <ModalContent>
-              <Answer>
-                <AnswerTitle>Dependente</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('dressUp');
-                    setDressUpScore(0);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>
-                  Precisa de ajuda mas consegue fazer uma parte sozinho
-                </AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('dressUp');
-                    setDressUpScore(10);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>
-                  Independente (incluindo botões, zipers, laços, etc.)
-                </AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('dressUp');
-                    setDressUpScore(10);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
+                <ScaleAnswerModal answerTitle='Dependente' onPress={
+                  () => {
+                      toggleModal('dressUp');
+                      setDressUpScore(0);
+                }} />
+                <ScaleAnswerModal answerTitle='Precisa de ajuda mas consegue fazer uma parte sozinho' onPress={() => {
+                      toggleModal('dressUp');
+                      setDressUpScore(5);
+                    }}/>
+                <ScaleAnswerModal answerTitle='Independente' onPress={() => {
+                  toggleModal('dressUp');
+                  setDressUpScore(10);
+                }}/>
             </ModalContent>
           </Modal>
+
           <Modal
             isVisible={isIntestineModalVisible}
             style={{ margin: 0 }}
             onBackdropPress={() => setIntestineModalVisible(false)}
           >
             <ModalContent>
-              <Answer>
-                <AnswerTitle>Incontinente (necessidade de enemas)</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('intestine');
-                    setIntestineScore(0);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>Acidente ocasional</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('intestine');
-                    setIntestineScore(5);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>Continente</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('intestine');
-                    setIntestineScore(10);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
+                <ScaleAnswerModal answerTitle='Incontinente (necessidade de enemas)' 
+                onPress={() => {
+                      toggleModal('intestine');
+                      setIntestineScore(0);
+                }} />
+                <ScaleAnswerModal answerTitle='Acidente ocasional' 
+                onPress={() => {
+                      toggleModal('intestine');
+                      setIntestineScore(5);
+                    }}/>
+                <ScaleAnswerModal answerTitle='Continente' 
+                onPress={() => {
+                  toggleModal('intestine');
+                  setIntestineScore(10);
+                }}/>
             </ModalContent>
           </Modal>
+
           <Modal
             isVisible={isUrinaryModalVisible}
             style={{ margin: 0 }}
             onBackdropPress={() => setUrinaryModalVisible(false)}
           >
             <ModalContent>
-              <Answer>
-                <AnswerTitle>
-                  Incontinente, ou cateterizado e incapaz de manejo
-                </AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('urinary');
-                    setUrinaryScore(0);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>Acidente ocasional</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('urinary');
-                    setUrinaryScore(5);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
-              <Answer>
-                <AnswerTitle>Continente</AnswerTitle>
-                <TouchableWithoutFeedback
-                  onPress={() => {
-                    toggleModal('urinary');
-                    setUrinaryScore(10);
-                  }}
-                >
-                  <SelectAnswerButton>
-                    <MaterialIcons
-                      name="radio-button-unchecked"
-                      size={24}
-                      color="black"
-                    />
-                  </SelectAnswerButton>
-                </TouchableWithoutFeedback>
-              </Answer>
+                <ScaleAnswerModal answerTitle='Incontinente, ou cateterizado e incapaz de manejo' 
+                onPress={() => {
+                      toggleModal('urinary');
+                      setUrinaryScore(0);
+                }} />
+                <ScaleAnswerModal answerTitle='Acidente ocasional' 
+                onPress={() => {
+                      toggleModal('urinary');
+                      setUrinaryScore(5);
+                    }}/>
+                <ScaleAnswerModal answerTitle='Continente' 
+                onPress={() => {
+                  toggleModal('urinary');
+                  setUrinaryScore(10);
+                }}/>
             </ModalContent>
           </Modal>
+
         </AccordionContainer>
       </ScrollView>
     </IndexContainer>
